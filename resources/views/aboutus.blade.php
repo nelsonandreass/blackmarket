20a6d4e04a12395771dd3eabdcfe7fed71dc1238
Nelson <nelson@sgo.co.id>
2020-10-22 20:36:18 +0700

    first

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <img src="" alt="image" class="text-center">
+                <br>
+                <h4 class="text-center">Pemakaman Online</h4>
+                <br><br><br>
+
+                <p class="text-center justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, vitae. Reprehenderit, facilis. Qui illum est ipsam pariatur adipisci unde nobis, aliquam, quibusdam harum nesciunt at ratione blanditiis exercitationem ipsum maiores.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, vitae. Reprehenderit, facilis. Qui illum est ipsam pariatur adipisci unde nobis, aliquam, quibusdam harum nesciunt at ratione blanditiis exercitationem ipsum maiores.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, vitae. Reprehenderit, facilis. Qui illum est ipsam pariatur adipisci unde nobis, aliquam, quibusdam harum nesciunt at ratione blanditiis exercitationem ipsum maiores.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, vitae. Reprehenderit, facilis. Qui illum est ipsam pariatur adipisci unde nobis, aliquam, quibusdam harum nesciunt at ratione blanditiis exercitationem ipsum maiores.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, vitae. Reprehenderit, facilis. Qui illum est ipsam pariatur adipisci unde nobis, aliquam, quibusdam harum nesciunt at ratione blanditiis exercitationem ipsum maiores.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, vitae. Reprehenderit, facilis. Qui illum est ipsam pariatur adipisci unde nobis, aliquam, quibusdam harum nesciunt at ratione blanditiis exercitationem ipsum maiores.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, vitae. Reprehenderit, facilis. Qui illum est ipsam pariatur adipisci unde nobis, aliquam, quibusdam harum nesciunt at ratione blanditiis exercitationem ipsum maiores.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, vitae. Reprehenderit, facilis. Qui illum est ipsam pariatur adipisci unde nobis, aliquam, quibusdam harum nesciunt at ratione blanditiis exercitationem ipsum maiores.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, vitae. Reprehenderit, facilis. Qui illum est ipsam pariatur adipisci unde nobis, aliquam, quibusdam harum nesciunt at ratione blanditiis exercitationem ipsum maiores.</p>
+            </div>
+            
+        </div>
+    </div>
+    
+@endsection
