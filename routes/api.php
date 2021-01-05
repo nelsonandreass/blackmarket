fc6ddd895d9f6d6fe2f3e120bce73c5dbe630638
Nelson <nelson@sgo.co.id>
2021-01-05 13:05:37 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/send' , 'TestController@send');
-Route::post('/loginprocess' , 'TestController@loginprocess');
+
